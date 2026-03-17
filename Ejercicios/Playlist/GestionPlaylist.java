022e6cc7c0d79ee9177284892d7737ac4823f4fd
Johny <angel_neet@outlook.com>
2026-03-16 22:02:33 -0600

    Ingreso de clase de gestion para indicaciones

--- Ejercicios/Playlist/GestionPlaylist.java
+++ Ejercicios/Playlist/GestionPlaylist.java
@@ -1,5 +1,48 @@
 package Ejercicios.Playlist;
+import java.util.Scanner;
 
 public class GestionPlaylist {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String menu ="1.- Agregar canción" +
+                     "\n2.- Eliminar canción por posición" +
+                     "\n3.- Mostrar toda la playlist" +
+                     "\n4.- Marcar canción como favorita" +
+                     "\n5.- Mostrar solo favoritas" +
+                     "\n6.- Buscar canciones por artista" +
+                     "\n7.- Duración total de la playlist" +
+                     "\n8.- ¿Cuántas canciones tengo?" +
+                     "\n9.- Salir";
+        
+        int opcion;
+
+        do{
+            System.out.println(menu + "\n Escoje una opcion: ");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9: System.out.println("Saliendo...");
+                    break;
+                default:
+                    break;
+            }
+        } while(opcion != 9);
+        sc.close();
+    }
 }
