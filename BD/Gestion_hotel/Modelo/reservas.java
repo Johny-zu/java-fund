aa97905949db2f9aaefdb7d8e15673468b044e0a
Johny <angel_neet@outlook.com>
2026-03-24 22:33:52 -0600

    Ingreso de clase estado e ingreso de reservas sin completar

---
+++ BD/Gestion_hotel/Modelo/reservas.java
@@ -0,0 +1,8 @@
+package BD.Gestion_hotel.Modelo;
+import java.time.LocalDate;
+
+public class reservas {
+    private int id_reserva;
+    private LocalDate fecha_inicio, fecha_fin, fecha_reserva;
+    
+}
