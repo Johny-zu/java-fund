2a5765282c40ec0c04998c8a15a67871c9db8751
Johny <angel_neet@outlook.com>
2026-03-29 13:56:16 -0600

    Squash Commit: Ingreso de clase FuncionReservas

--- BD/Gestion_hotel/FuncionesHotel/FuncionReservas.java
+++ BD/Gestion_hotel/FuncionesHotel/FuncionReservas.java
@@ -1,5 +1,6 @@
 package BD.Gestion_hotel.FuncionesHotel;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import BD.Gestion_hotel.ConexionBaseDatos;
 import BD.Gestion_hotel.Modelo.Estado;
+import BD.Gestion_hotel.Modelo.Huesped;
 import BD.Gestion_hotel.Modelo.Reservas;
 
 public class FuncionReservas {
@@ -25,7 +27,7 @@ public class FuncionReservas {
                     LocalDate fechaFin = sqlDateFin.toLocalDate();
                     java.sql.Timestamp  sqlDateReserva = rs.getTimestamp("fecha_reserva");
                     LocalDateTime fechaReserva = sqlDateReserva.toLocalDateTime();
-                        
+
                     Reservas r = new Reservas(
                         rs.getInt("id_reserva"),
                         fechaInicio,
@@ -38,5 +40,141 @@ public class FuncionReservas {
                 }
             }
         return listadoReservas;
+    }
+
+    public List<Reservas> enlistarPorEstado(String estado) throws SQLException{
+        List<Reservas> listaEstados = new ArrayList<>();
+        String sql = "SELECT * FROM reservas WHERE estado=?";
+        try(Connection conn = ConexionBaseDatos.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){  
+            pstmt.setString(1, estado);  
+            try(ResultSet rs = pstmt.executeQuery()){  
+                while (rs.next()) {
+                    java.sql.Date sqlDateInicio = rs.getDate("fecha_inicio");
+                    LocalDate fechaInicio = sqlDateInicio.toLocalDate();
+                    java.sql.Date sqlDateFin = rs.getDate("fecha_fin");
+                    LocalDate fechaFin = sqlDateFin.toLocalDate();
+                    java.sql.Timestamp sqlDateReserva = rs.getTimestamp("fecha_reserva");
+                    LocalDateTime fechaReserva = sqlDateReserva.toLocalDateTime();
+                    Reservas r = new Reservas(
+                        rs.getInt("id_reserva"),
+                        fechaInicio,
+                        fechaFin,
+                        fechaReserva,
+                        Estado.fromString(rs.getString("estado")),
+                        rs.getDouble("total")
+                    );
+                    listaEstados.add(r);
+                }
+            }
+        }
+        return listaEstados;
+    }
+
+    public List<Reservas> enlistarPorHuesped(Huesped huesped) throws SQLException{
+        List<Reservas> listaHuespedes = new ArrayList<>();
+        String sql = "SELECT * FROM reservas WHERE id_huesped=?";
+        try(Connection conn = ConexionBaseDatos.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){  
+            pstmt.setInt(1, huesped.getId_huesped());  
+            try(ResultSet rs = pstmt.executeQuery()){  
+                while (rs.next()) {
+                    java.sql.Date sqlDateInicio = rs.getDate("fecha_inicio");
+                    LocalDate fechaInicio = sqlDateInicio.toLocalDate();
+                    java.sql.Date sqlDateFin = rs.getDate("fecha_fin");
+                    LocalDate fechaFin = sqlDateFin.toLocalDate();
+                    java.sql.Timestamp sqlDateReserva = rs.getTimestamp("fecha_reserva");
+                    LocalDateTime fechaReserva = sqlDateReserva.toLocalDateTime();
+                    Reservas r = new Reservas(
+                        rs.getInt("id_reserva"),
+                        fechaInicio,
+                        fechaFin,
+                        fechaReserva,
+                        Estado.fromString(rs.getString("estado")),
+                        rs.getDouble("total")
+                    );
+                    listaHuespedes.add(r);
+                }
+            }
+        }
+        return listaHuespedes;
+    }
+
+    public List<Reservas> enlistarPorFechas(LocalDate fecha_inicio, LocalDate fecha_fin) throws SQLException {
+        List<Reservas> listadoPorFechas = new ArrayList<>();
+        String sql = "SELECT * FROM reservas WHERE fecha_inicio >= ? AND fecha_fin <= ?";
+        try (Connection conn = ConexionBaseDatos.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                java.sql.Date sqlFechaInicio = java.sql.Date.valueOf(fecha_inicio);
+                java.sql.Date sqlFechaFin = java.sql.Date.valueOf(fecha_fin);
+                
+                pstmt.setDate(1, sqlFechaInicio);
+                pstmt.setDate(2, sqlFechaFin);
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    while (rs.next()) {
+                        java.sql.Date sqlDateInicio = rs.getDate("fecha_inicio");
+                        LocalDate fechaInicio = sqlDateInicio.toLocalDate();
+                        java.sql.Date sqlDateFin = rs.getDate("fecha_fin");
+                        LocalDate fechaFin = sqlDateFin.toLocalDate();
+                        java.sql.Timestamp sqlDateReserva = rs.getTimestamp("fecha_reserva");
+                        LocalDateTime fechaReserva = sqlDateReserva.toLocalDateTime();
+
+                        Reservas r = new Reservas(
+                            rs.getInt("id_reserva"),
+                            fechaInicio,
+                            fechaFin,
+                            fechaReserva,
+                            Estado.fromString(rs.getString("estado")),
+                            rs.getDouble("total")
+                        );
+                        listadoPorFechas.add(r);
+                    }
+                }
+            }
+        return listadoPorFechas;
+    }
+
+    public void insertar(Reservas reservas) throws SQLException{
+        String sql = "INSERT INTO reservas (id_huesped, fecha_inicio, fecha_fin, fecha_reserva, estado, total) VALUES (?, ?, ?, ?, ?, ?)";
+        try(Connection conn = ConexionBaseDatos.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.setInt(1, reservas.getHuesped().getId_huesped());
+                pstmt.setDate(2, java.sql.Date.valueOf(reservas.getFecha_inicio()));
+                pstmt.setDate(3, java.sql.Date.valueOf(reservas.getFecha_fin()));
+                pstmt.setTimestamp(4, java.sql.Timestamp.valueOf(reservas.getFecha_reserva()));
+                pstmt.setString(5, reservas.getEstado().getValor());
+                pstmt.setDouble(6, reservas.getTotal());
+                
+                pstmt.executeUpdate();
+        }
+    }
+
+    public void actualizar(Reservas reservas) throws SQLException{
+        String sql = "UPDATE reservas SET id_huesped=?, fecha_inicio=?, fecha_fin=?, fecha_reserva=?, estado=?, total=? WHERE id_reserva=?";
+        try(Connection conn = ConexionBaseDatos.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.setInt(1, reservas.getHuesped().getId_huesped());
+                pstmt.setDate(2, java.sql.Date.valueOf(reservas.getFecha_inicio()));
+                pstmt.setDate(3, java.sql.Date.valueOf(reservas.getFecha_fin()));
+                pstmt.setTimestamp(4, java.sql.Timestamp.valueOf(reservas.getFecha_reserva()));
+                pstmt.setString(5, reservas.getEstado().getValor());
+                pstmt.setDouble(6, reservas.getTotal());
+                pstmt.setInt(7, reservas.getId_reserva());  
+                pstmt.executeUpdate();
+        }
+    }
+
+    public void eliminarPorID(int id) throws SQLException{
+        String sql = "DELETE FROM reservas WHERE id_huesped=?";
+        try(Connection conn = ConexionBaseDatos.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.setInt(1, id);
+                int filaAfectada = pstmt.executeUpdate();
+                if (filaAfectada > 0) {
+                    System.out.println("Reserva eliminada con exito");
+                } else {
+                    System.out.println("No se hallo la reserva ingresada");
+                }
+            }
     }
 }
