2e34b4da3cf5f315718af18772caa36bfd8a9704
Johny <angel_neet@outlook.com>
2026-04-04 19:57:20 -0600

    Fix: redireccion BD

--- BD/Gestion_hotel/ConexionBaseDatos.java
+++ BD/Gestion_hotel/ConexionBaseDatos.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBaseDatos {
-    private static final String URL = "jdbc:mysql://localhost:3306/personas";
+    private static final String URL = "jdbc:mysql://localhost:3306/hoteles";
     private static final String USER = "root"; 
     private static final String PASSWORD = "0000";
 
